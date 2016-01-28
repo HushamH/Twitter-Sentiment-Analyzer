446ed8b1a2ae509b48e61cf9a602d7a79de67877
HushamH <husham.hassan@mail.utoronto.ca>
2016-01-27 23:10:11 -0500

    Added the barebones for twtt.py

--- twtt.py
+++ twtt.py
@@ -1,4 +1,61 @@
 """
 This is the twtt module. It is responsible for tokenizing and tagging tweets 
 from the csv files taken from the Sentiment140 Corpus.
+
+The program takes in a tweet file, following the form specified in the
+Assignment guidelines and convert it to the normalized form which is also
+specified in the guidelines.
 """
+
+""" Remove any HTML tags and attributes. """
+def remove_html_tag_and_attr():
+	return
+
+""" Replace any HTML character code with ASCII equivalent. """
+def replace_html_char_code():
+	return
+
+""" Remove URLs (tokens beginning with www/http). """
+def remove_urls():
+	return
+
+""" Remove the first char of twitter usernames '@' and hastags '#'. """
+def remove_at_and_hash():
+	return
+
+""" Remove the first char of twitter usernames '@' and hastags '#'. """
+def remove_at_and_hash():
+	return
+
+""" Place every sentence within a tweet on its own line. """
+def sentence_new_line():
+	return
+
+""" Keep Ellipsis and other multiple punctuations together. """
+def ellip_and_mult():
+	return
+
+# TO-DO: This may not be a function, this may be something we incorporate
+# 		 into our other functions
+""" Keep Ellipsis and other multiple punctuations together. """
+def ellip_and_mult():
+	return
+
+""" Make each token, including clitics, separated by whitespace. """
+def add_whitespace():
+	return
+
+""" Tag each token with its associated PoS. """
+def POS_tag():
+	return
+
+""" Add the appropriate demarcation before each tweet including tweet class. """
+def add_demarcation():
+	return
+
+""" Pre-process the tweets """
+def pre_process():
+	return
+
+if __name__ == "__main__":
+	pre_process <filename> <group Number> N
