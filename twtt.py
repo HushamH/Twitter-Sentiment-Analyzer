95f6ec250601e54e4bb06455ed8e0dfd88dbfffd
chojohn <hoyeon517@gmail.com>
2016-02-09 16:23:59 -0500

    twtt starter

--- twtt.py
+++ twtt.py
@@ -1,5 +1,9 @@
+import sys
+import fileinput
+import re
+
 """
-This is the twtt module. It is responsible for tokenizing and tagging tweets 
+This is the twtt module. It is responsible for tokenizing and tagging tweets
 from the csv files taken from the Sentiment140 Corpus.
 
 The program takes in a tweet file, following the form specified in the
@@ -7,12 +11,15 @@ Assignment guidelines and convert it to the normalized form which is also
 specified in the guidelines.
 """
 
+
 """ Remove any HTML tags and attributes. """
 def remove_html_tag_and_attr():
 	return
 
 """ Replace any HTML character code with ASCII equivalent. """
 def replace_html_char_code():
+	# use re to find the html code then use unichr() to convert 
+	# then use replace() to change it
 	return
 
 """ Remove URLs (tokens beginning with www/http). """
@@ -58,4 +65,58 @@ def pre_process():
 	return
 
 if __name__ == "__main__":
-	pre_process <filename> <group Number> N
+	#pre_process <filename> <group Number> N
+
+
+	#### what i have so far:
+	#### 	- handles arguments and puts them in the correct variables
+	#### 	- goes through the file line by line and separates only the tweet part
+	####	- separated line are output in the output file
+
+	# Argument Handling
+	if len(sys.argv) == 3:
+		# has NO group ID
+		print 'Number of arguments:', len(sys.argv), 'arguments.'
+		input_file = str(sys.argv[1])
+		output_file = str(sys.argv[2])
+		print 'Input file :', input_file
+		print 'Output file :', output_file
+	elif len(sys.argv) == 4:
+		# has group ID
+		print 'Number of arguments:', len(sys.argv), 'arguments.'
+		input_file = str(sys.argv[1])
+		group_id = str(sys.argv[2])
+		output_file = str(sys.argv[3])
+		print 'Input file :', input_file
+		print 'Output file :', output_file
+		print 'Group ID :', group_id
+	else:
+		# Incorrect amount of arguments
+		print "Incorrect amount of arguments"
+
+	i_count = 0
+	c_count = 0
+	index = 0
+
+	# Open output file
+	f = open(output_file,'w')
+
+	# Read Line from File and separates tweet
+	for line in fileinput.input([input_file]):
+		for char in line:
+			c_count += 1
+			if char == ",":
+				i_count += 1
+			if i_count == 4:
+				index = c_count
+		print line[index+1:]
+		f.write(line[index+1:])
+		c_count = 0
+		i_count = 0
+		index = 0
+
+	# Close output file
+	f.close()
+
+
+
